837dbc2003d847933541af3d92dd88009a5a8253
PN1111 <114814059+PN1111@users.noreply.github.com>
2022-10-16 17:46:34 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,17 +25,23 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 
+# New section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-# import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
-# streamlit.text(fruityvice_response)
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  if not fruit_choice: 
+    streamlit.error("Please select a fruit to get information.")
+  else 
+    # import requests
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+    # streamlit.text(fruityvice_response)
+    # write your own comment -what does the next line do? 
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # write your own comment - what does this do?
+    s.treamlit.dataframe(fruityvice_normalized)
+   
+Except URLError as e:
+    streamlit.error()
 
 # dont run anythyng past here while while we troubleshoot
 streamlit.stop()
